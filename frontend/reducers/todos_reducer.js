187654b4ce4cf6f4052351ac7fa5c051373c4c95
manjoeso <jmanso93@gmail.com>
2021-12-14 15:10:10 -0500

    working on remove todo

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,7 +1,36 @@
-const todosReducer = (state = {}, action) => {
+import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO } from "../actions/todo_actions.js"
+
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+
+const todosReducer = (prevState = initialState, action) => {
+    Object.freeze(prevState);
+    const nextState = Object.assign({}, prevState);
+
     switch (action.type) {
+      case RECEIVE_TODOS:
+        return action.todos;
+      case RECEIVE_TODO:
+        nextState[action.todo.id] = action.todo;
+        return nextState; 
+      case REMOVE_TODO:
+        nextState.removeFromObjectByKey(action.todo.id)
+        return nextState;
       default:
-        return state;
+        return prevState;
     }
 };
   
