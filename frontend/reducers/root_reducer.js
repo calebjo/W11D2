f378a5b3104e4418eb08d47a0fef2b621e261576
calebjo <caleb.ri.jones@gmail.com>
2021-12-14 14:24:48 -0500

    Create initial redux setup

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import todosReducer from './todos_reducer';
+import todoReducer from './todos_reducer';
+
+const rootReducer = combineReducers({
+    todos: todosReducer,
+    todo: todoReducer
+})
+
+export default rootReducer;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(<h1>Hello!</h1>, document.getElementById('content'));
+    const store = configureStore();
+    window.store = store;
+    window.receiveTodos = receiveTodos;
+    window.receiveTodo = receiveTodo;
 });
