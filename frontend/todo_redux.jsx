97c4385281dc76d77a35092c43604b5aa35fc387
manjoeso <jmanso93@gmail.com>
2021-12-15 14:24:39 -0500

    created selector in phase 2

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { receiveTodos, receiveTodo, removeTodo } from './actions/todo_actions';
 import { receiveSteps, receiveStep, removeStep } from './actions/step_actions';
 import configureStore from './store/store';
+import Root from './components/root.jsx';
+import allTodos from './reducers/selectors.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<h1>Hello!</h1>, document.getElementById('content'));
     const store = configureStore();
+    ReactDOM.render(<Root store={store}/>, document.getElementById('content'));
     window.store = store;
     window.removeTodo = removeTodo;
     window.receiveTodos = receiveTodos;
@@ -15,4 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
     window.removeStep = removeStep;
     window.receiveSteps = receiveSteps;
     window.receiveStep = receiveStep;
+
+    window.allTodos = allTodos;
 });
