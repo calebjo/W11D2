5eded17507d4a006bea811872a725c80933ed700
manjoeso <jmanso93@gmail.com>
2021-12-16 16:59:14 -0500

    added rails project!

--- todos/db/seeds.rb
+++ todos/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+
+Todo.destroy_all
+
+todo1 = Todo.create!(title: "Buy melons", body: "Yummy melons", done: false)
+todo2 = Todo.create!(title: "Buy banana", body: "Yummy bananas", done: false)
+todo3 = Todo.create!(title: "purchase onion", body: "Yummy onions", done: false)
+
+end
